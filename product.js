fccd6071fd59a4c335894eae4dcf39c88d823718
Aashutoshkumar2252 <aashutoshkumarsingh33@gmail.com>
2025-05-28 15:25:59 +0530

    change styling

--- product.js
+++ product.js
@@ -1,29 +1,3 @@
-/*
-
-document.addEventListener("DOMContentLoaded", () => {
-    // Get Product ID from URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const productId = urlParams.get("id");
-
-    // Fetch JSON Data
-    fetch("products.json")
-        .then(response => response.json())
-        .then(products => {
-            const product = products.find(p => p.id == productId);
-            if (product) {
-                document.getElementById("product-img").src = product.image;
-                document.getElementById("product-name").textContent = product.name;
-                document.getElementById("product-description").textContent = product.description;
-                document.getElementById("product-price").textContent = product.price;
-            } else {
-                document.querySelector(".product-info").innerHTML = "<h2>Product not found.</h2>";
-            }
-        })
-        .catch(error => console.error("Error loading product data:", error));
-});
-*/
-
-
 document.addEventListener("DOMContentLoaded", () => {
   fetch("products.json")
     .then(response => response.json())
