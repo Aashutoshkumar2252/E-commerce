acad39d6c8488d66cdb175235727abbd0ec4c66b
Aashutoshkumar2252 <aashutoshkumarsingh33@gmail.com>
2025-05-29 08:09:59 +0530

    image repair

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // ----- Product Mock Data -----
 const products = [
-  { id: 1, name: "Air Max", price: "$120", image: "air-max.png" },
+  { id: 1, name: "Air Max Pro", price: "$120", image: "air-max pro.png" },
   { id: 2, name: "Ultraboost", price: "$140", image:"ultra-boost.png" },
   { id: 3, name: "RS-X", price: "$100", image: "RS-X.png" },
   { id: 4, name: "Classic", price: "$90", image: "classic.png" }
